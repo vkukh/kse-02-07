17c6cd8d24bc9ed278656f76b46da90880c6750e
Vitalii Kukhar <vitalii_kukhar@epam.com>
2021-11-29 21:43:46 +0200

    Implemented sum fn with c++

---
+++ 2-sum-for-loop.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+ 
+int sum(int n, ...) {
+    int result = 0;
+
+    va_list factor;
+    va_start(factor, n);
+
+    for(int i=0;i<n; i++) {
+        result += va_arg(factor, int);
+    }
+
+    va_end(factor);
+
+    return result;
+}
+ 
+int main() {   
+    printf("%d \n", sum(4, 1, 2, 3, 4));
+    printf("%d \n", sum(5, 12, 21, 13, 4, 5));
+}
